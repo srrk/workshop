90927f3aea024400772f0c9da999c26e84037a2f
SR Rajesh Kumar <rajeshux@gmail.com>
2017-03-07 13:25:36 +0530

    Changed the HTML for the site.

--- workshop/urls.py
+++ workshop/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^workshop/', include('registration.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
